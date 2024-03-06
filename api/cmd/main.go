d1755be498450026f06f8b5157a9d0c2dee6cc43
Tormod Mork Müller <60174150+Tormodmm@users.noreply.github.com>
2024-03-06 19:58:34 +0100

    init setup api

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func itemHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id") // Dynamically access the path variable
+	fmt.Fprintf(w, "Retrieving item with ID: %s", id)
+}
+
+func filesHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.PathValue("path") // Accessing the wildcard path variable
+	fmt.Fprintf(w, "Accessing file at: %s", path)
+}
 
 func main() {
-	fmt.Println("Up and Running!")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/items/{id}", itemHandler)
+	mux.HandleFunc("/files/{path...}", filesHandler)
+
+	http.ListenAndServe(":8080", mux)
 }
