329e91861930597ed0dcbaf0714b99de543ac9d8
Tormod Mork Müller <60174150+Tormodmm@users.noreply.github.com>
2024-03-08 14:52:00 +0100

    add line chart updates
    add line chart updates which make it possible to select and deselect lines

--- webapp/src/Components/LineChartCustom/LineChartCustom.js
+++ webapp/src/Components/LineChartCustom/LineChartCustom.js
@@ -1,32 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, Tooltip, Legend, YAxis, ResponsiveContainer } from 'recharts';
 import './LineChartCustom.css';
-import {LineChart, Line, CartesianGrid, XAxis, Tooltip, Legend, YAxis, ResponsiveContainer, Label} from 'recharts';
 
-function LineChartCustom({data}) {
+function LineChartCustom({ data }) {
+    const generateVisibleLines = (data) => {
+        const visibleLines = {};
+        data.forEach(item => {
+            Object.keys(item).forEach(key => {
+                visibleLines[key] = true;
+            });
+        });
+        return visibleLines;
+    };
+
+    const [visibleLines, setVisibleLines] = useState(generateVisibleLines(data));
+
+    const [lastToggled, setLastToggled] = useState({ vendor: null, isVisible: false });
+
+    const toggleLineVisibility = (vendor) => {
+        const isVisible = !visibleLines[vendor];
+        setVisibleLines(prevState => ({
+            ...prevState,
+            [vendor]: isVisible,
+        }));
+        // Track the last toggled line and its visibility
+        setLastToggled({ vendor, isVisible });
+    };
+
+    const colors = {
+        Vendor_1: '#A4D4B4',
+        Vendor_2: '#463F3A',
+        Vendor_3: '#F7934C',
+        Vendor_4: '#CC5803',
+        Vendor_5: '#1F1300',
+    }
 
     return (
-        <div className={"outerContainer"}>
-            <ResponsiveContainer width="100%" height="100%">
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip cursor={{ stroke: 'gray', strokeWidth: 1 }} />
-                    <Legend />
-                    <Line type="bump" dataKey="Vendor_1" strokeWidth={2} stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="bump" dataKey="Vendor_2" strokeWidth={2} stroke="#82ca9d" activeDot={{ r: 5 }} />
-                </LineChart>
-            </ResponsiveContainer>
+        <div className={"container-outer"}>
+            <div className={"container-chart"}>
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip cursor={{ stroke: 'gray', strokeWidth: 1 }} />
+                        <Legend />
+
+                        {Object.keys(visibleLines).map(vendor => (
+                            visibleLines[vendor] && ( // Only render the line if its visibility is true
+                                <Line
+                                    key={vendor}
+                                    type="bump"
+                                    dataKey={vendor}
+                                    strokeWidth={2}
+                                    stroke={colors[vendor]}
+                                    activeDot={{ r: 8 }}
+                                    isAnimationActive={lastToggled.vendor === vendor} // Ensure this line remains for controlling animation
+                                />
+                            )
+                        ))}
+
+                    </LineChart>
+                </ResponsiveContainer>
+
+                <div className="container-chart-buttons">
+                    {Object.keys(visibleLines).map(vendor => (
+                        <div key={vendor} className="vendor-option">
+                            <button
+                                className={`button ${visibleLines[vendor] ? 'button-active' : ''}`}
+                                onClick={() => toggleLineVisibility(vendor)}
+                            />
+                            <span className="vendor-text">{vendor}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
--- webapp/src/Pages/ProjectSummary/ProjectSummary.js
+++ webapp/src/Pages/ProjectSummary/ProjectSummary.js
@@ -8,61 +8,90 @@ function ProjectSummary() {
             name: 'SC 1',
             Vendor_1: 4,
             Vendor_2: 5,
+            Vendor_3: 6,
+            Vendor_4: 7,
+            Vendor_5: 5,
         },
         {
             name: 'SC 2',
             Vendor_1: 7,
             Vendor_2: 6,
+            Vendor_3: 5,
+            Vendor_4: 7,
+            Vendor_5: 7
         },
         {
             name: 'SC 3',
             Vendor_1: 2,
             Vendor_2: 3,
+            Vendor_3: 4,
+            Vendor_4: 3,
+            Vendor_5: 4
         },
         {
             name: 'SC 4',
             Vendor_1: 7,
             Vendor_2: 9,
+            Vendor_3: 7,
+            Vendor_4: 8,
+            Vendor_5: 9
         },
         {
             name: 'SC 5',
             Vendor_1: 7,
             Vendor_2: 7,
+            Vendor_3: 8,
+            Vendor_4: 9,
+            Vendor_5: 8
         },
         {
             name: 'SC 6',
             Vendor_1: 4,
             Vendor_2: 5,
+            Vendor_3: 6,
+            Vendor_4: 5,
+            Vendor_5: 6
         },
         {
             name: 'SC 7',
             Vendor_1: 9,
             Vendor_2: 7,
+            Vendor_3: 8,
+            Vendor_4: 7,
+            Vendor_5: 8
         },
         {
             name: 'SC 8',
             Vendor_1: 8,
             Vendor_2: 3,
+            Vendor_3: 4,
+            Vendor_4: 5,
+            Vendor_5: 6
         },
         {
             name: 'SC 9',
             Vendor_1: 7,
             Vendor_2: 8,
+            Vendor_3: 9,
+            Vendor_4: 8,
+            Vendor_5: 7
         },
         {
             name: 'SC 10',
             Vendor_1: 6,
             Vendor_2: 7,
+            Vendor_3: 6,
+            Vendor_4: 5,
+            Vendor_5: 5
         }
     ];
 
-
-  return (
-    <div>
-      <h1>Project Summary</h1>
-        <LineChartCustom data={data}/>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Project Summary</h1>
+            <LineChartCustom data={data}/>
+        </div>
+    );
 }
 
 export default ProjectSummary;
