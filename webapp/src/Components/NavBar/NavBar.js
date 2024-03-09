2e5e99c3304dffa35909fbac4a5e4ba23e66a2fe
Tormod Mork Müller <60174150+Tormodmm@users.noreply.github.com>
2024-03-09 12:21:37 +0100

    add navigation bar for application

---
+++ webapp/src/Components/NavBar/NavBar.js
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+import { useState } from "react"
+import AppLogo from "../../SVGs/app-logo.svg"
+
+export default function NavBar() {
+  const navBarHeight = 44
+  let pathFolder = "project"
+  let pathSubFolderCategoryOne = "setup"
+  let pathSubFolderCategoryTwo = "summary"
+  let pathSubFolderCategoryThree = "decision making"
+
+  const [, setSelectedCategory] = useState(null)
+
+  const handleCategoryItemClick = (category) => {
+    // Handle the selection of a category item
+    setSelectedCategory(category)
+  }
+
+  return (
+    <div className={`page-container`}>
+      <div
+        className="headerContainer"
+      >
+        <nav
+          className={`globalNavbar navHeight-${navBarHeight}`}
+        >
+          <div className="column-container">
+            <div className={"row-container"}>
+              <div className="navMainButton">
+                <Link to="/" onClick={() => handleCategoryItemClick("home")}>
+                  <div className={"category-item"}>
+                    <img className={"app-logo"} src={AppLogo} alt={"logo"}/>
+                  </div>
+                </Link>
+              </div>
+              <div className="navMainButton">
+                <Link
+                    to={`/${pathFolder}/${pathSubFolderCategoryOne}`}
+                    onClick={() => handleCategoryItemClick(pathSubFolderCategoryOne)}
+                >
+                  <div className={"category-item"}>{pathSubFolderCategoryOne}</div>
+                </Link>
+              </div>
+              <div className="navMainButton">
+                <Link
+                    to={`/${pathFolder}/${pathSubFolderCategoryTwo}`}
+                    onClick={() => handleCategoryItemClick(pathSubFolderCategoryTwo)}
+                >
+                  <div className={"category-item"}>{pathSubFolderCategoryTwo}</div>
+                </Link>
+              </div>
+              <div className="navMainButton">
+                <Link
+                    to={`/${pathFolder}/${pathSubFolderCategoryThree}`}
+                    onClick={() => handleCategoryItemClick(pathSubFolderCategoryThree)}
+                >
+                  <div className={"category-item"}>{pathSubFolderCategoryThree}</div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  )
+}
