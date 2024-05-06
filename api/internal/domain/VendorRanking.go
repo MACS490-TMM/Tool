423cd003a2f9d68719482fbcc85b7fd2f75e7664
Tormod Mork Müller <60174150+Tormodmm@users.noreply.github.com>
2024-05-06 12:41:02 +0200

    Modify to take fuzzy ranking format form algorithm

--- api/internal/domain/VendorRanking.go
+++ api/internal/domain/VendorRanking.go
@@ -1,8 +1,8 @@
 package domain
 
 type VendorRanking struct {
-	ProjectID int `json:"projectId"`
-	VendorID  int `json:"vendorId"`
-	Ranking   int `json:"ranking"`
-	Score     int `json:"score"`
+	ProjectID int     `json:"projectId"`
+	VendorID  int     `json:"vendorId"`
+	Ranking   int     `json:"ranking"`
+	Score     float64 `json:"score"`
 }
